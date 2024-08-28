313319dd92fdaae8428c30040893a6b94a4ae48f
ctupac999 <cristiands9999@gmail.com>
2024-08-28 15:41:37 +0200

    structures better

--- src/styles.js
+++ src/styles.js
@@ -7,10 +7,13 @@ document.getElementById("cambiarEstilosBtn").addEventListener("click", function(
     if (estiloOriginal) {
         root.style.setProperty('--secondary-color', '#ffffff');
         root.style.setProperty('--primary-color', '#171718');
+        root.style.setProperty('--quinary-color', '#252525');
+
         boton.classList.add('activado');
     } else {
         root.style.setProperty('--secondary-color', '#171718');
         root.style.setProperty('--primary-color', '#ffffff');
+        root.style.setProperty('--quinary-color', '#eeeeee');
          boton.classList.remove('activado');
     }
 
