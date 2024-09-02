de67568e950ae012ae154e656ac7b3ae8506a3a8
ctupac999 <cristiands9999@gmail.com>
2024-09-02 19:02:02 +0200

    lenguaje ok

--- src/idiomas.js
+++ src/idiomas.js
@@ -10,18 +10,21 @@ const idiomas = document.getElementById('idiomas');
 const portadaTitulo = document.getElementById('portadaTitulo');
 const tituloBiografia = document.getElementById('tituloBiografia');
 const tituloProyectos = document.getElementById('tituloProyectos');
-const herramientasUtilizadas = document.getElementById('herramientasUtilizadas');
 const textoProyecto1 = document.getElementById('textoProyecto1');
-const buttonRepositorio = document.getElementsByClassName('buttonRepositorio');
-const buttoonPaginaWEb = document.getElementById('buttoonPaginaWEb');
-const repositorio1 = document.getElementById('buttonPrototipo');
+const buttonRepositorio1 = document.getElementById('buttonRepositorio1');
+const buttoonPaginaWEb1 = document.getElementById('buttoonPaginaWEb1');
+const buttonPrototipo1 = document.getElementById('buttonPrototipo1');
 const tituloHerramientas = document.getElementById('tituloHerramientas');
+const textoProyecto2 = document.getElementById('textoProyecto2');
+const buttonRepositorio2 = document.getElementById('buttonRepositorio2');
+const textoProyecto3 = document.getElementById('textoProyecto3');
+const buttonRepositorio3 = document.getElementById('buttonRepositorio3');
+const buttoonPaginaWEb3 = document.getElementById('buttoonPaginaWEb3');
 
 
 
 
-
-const saludos = [saludo2, saludo3, saludo4, saludo5, saludo6, textobio1, textobio2, textobio3, idiomas, portadaTitulo, tituloBiografia, tituloProyectos, herramientasUtilizadas, textoProyecto1, buttonRepositorio, buttoonPaginaWEb, buttonPrototipo, tituloHerramientas];
+const saludos = [saludo2, saludo3, saludo4, saludo5, saludo6, textobio1, textobio2, textobio3, idiomas, portadaTitulo, tituloBiografia, tituloProyectos, textoProyecto1, buttonRepositorio1, buttoonPaginaWEb1, buttonPrototipo1, tituloHerramientas, textoProyecto2, buttonRepositorio2, textoProyecto3, buttonRepositorio3, buttoonPaginaWEb3];
 
 const textosOriginales = [
     'o', 
@@ -36,12 +39,17 @@ const textosOriginales = [
     'Programmer and web designer',
     'Biography',
     'Relevant projects',
-    'Tools used',
     'This is a framework that interacts with a DB (in this case a .json to simulate it). This interface provides us with a list of data depending on the filter used, a graph and the indicators. Everything about this same DB. You can interact on the page and test it, in addition to checking the code on github through the following links!',
     'Repository',    
     'Web Page',
     'Prototype',
     'Tools',
+    'This is a project to create a Rest Api that interacts with a DB in the NoSQL cloud (in this case Mongodb Atlas). This repository contains everything you need to create your own server so that you can perform all CRUD operations with a real database and in the cloud. You can check the code on github via the links below!',
+    'Repository',
+    'This is a project to create a graphical interface accompanied by a Rest API that interacts with a DB in the NoSQL cloud (in this case Mongodb Atlas). The project consists of obtaining graphic and statistical results from a DB of an external client. You can check the code on github through the following links!',
+    'Repository',
+    'Web Page',
+
 ];
 const textosAlternativos = [
     'i', 
@@ -56,12 +64,17 @@ const textosAlternativos = [
     'Programador y diseñador web',
     'Briografía',
     'Proyectos relevantes',
-    'Herramientas utilizadas',
     'Este es un framework que interactúa con una DB (en este caso un .json para simularlo).Esta interfaz nos proporcinona una la lista de datos dependiendo el filtro utilizado, un gráfico y los indicadores. Todo sobre esta misma DB. Puedes interactuar en la página y probarla, además de checkear el código en github a través de los siguientes enlaces!',
     'Repositorio',    
     'Página Web',
     'Prototipo',
     'Herramientas',
+    'Este es proyecto de creación de una Api Rest que interactúa con una DB en la nube NoSQL (en este caso Mongodb Atlas). Este repositorio contiene todo lo que necesitas para crear tu propio servidor para que puedas realizar todas las operaciones CRUD con una base de datos real y en la nube. ¡Puedes verificar el código en github a través de los siguientes enlaces!',
+    'Repositorio',
+    'Este es proyecto de creación de una interfaz gráfica acompañada de una Api Rest que interactúa con una DB en la nube NoSQL(en este caso Mongodb Atlas). El proyecto consiste en la obtención de resultados gráficos y estadísticos de una DB de un cliente externo. Puedes checkear el código en github a través de los siguientes enlaces!',
+    'Repositorio',
+    'Página Web',
+
 ];
 
 function alternarTextos() {
